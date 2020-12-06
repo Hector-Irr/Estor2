91ea0804cd70fb9a9c39c6fd4746350244b53104
Hector <h.irrmann@laposte.net>
2020-12-06 11:46:04 +0100

    Recapitulatif infos (pas encore style)

--- JavaScript/Recapitulatif.js
+++ JavaScript/Recapitulatif.js
@@ -24,8 +24,45 @@ console.log("destination:"+destination)
 
 
 
+var vDestination=document.getElementById("destination");
+vDestination.innerHTML=destination
 
+var vNom=document.getElementById("nom");
+vNom.innerHTML=nom
 
+var vPrenom=document.getElementById("prenom");
+vPrenom.innerHTML=prenom
+
+var vAdulte=document.getElementById("adulte");
+vAdulte.innerHTML=adulte
+
+var vEnfant=document.getElementById("enfant");
+vEnfant.innerHTML=enfant
+
+var vEmail=document.getElementById("email");
+vEmail.innerHTML=email
+
+var vTelephone=document.getElementById("telephone");
+vTelephone.innerHTML=telephone
+
+var vDepart=document.getElementById("depart");
+vDepart.innerHTML=depart
+
+var vRetour=document.getElementById("retour");
+vRetour.innerHTML=retour
+
+var vRetour=document.getElementById("retour");
+vRetour.innerHTML=retour
+
+var vDejeuner=document.getElementById("dejeuner");
+if (dejeuner==false){
+    vDejeuner.innerHTML="Vous n'avez pas demandé de supplément petit-déjeuner."
+}else{
+    vDejeuner.innerHTML="Vous avez demandé un supplément petit-déjeuner, ce qui ajoute 12€ par personne au prix total."
+}
+
+var vPrix=document.getElementById("prix");
+vPrix.innerHTML=prix
 
 
 
